5d1b477acb901f6b202d8c0e96ebce052ade2fd5
WesKw <kwiecinskiwesley@gmail.com>
2026-01-31 17:18:24 -0600

    Fixed parsing of input files, added sample transactions file

--- msapriori.py
+++ msapriori.py
@@ -15,29 +15,48 @@ class ParameterData:
         to_str = (f"SDC = {self.support_difference_constraint}\n" + 
                   f"Min confidence = {self.min_confidence}\n")
         
+        for key,value in self.mis_per_item.items():
+            to_str += f"\nMIS({key}) = {value}"
+
+        return to_str
+        
 
 
-def parse_params_cfg(path: str) -> ParameterData:
-    data = ParameterData()
-
+def parse_params_cfg(path: str, param_data: ParameterData) -> ParameterData:
     with open(path) as params_file:
         for line in params_file:
             pps = "".join(line.split()).split("=") # remove whitespace and split by =
 
             if "MIS" in pps[0]:
-                items = re.findall(r"[\d.]+", pps[0] + pps[1]) # find both item name and MIS value
-                print(items)
-                data.mis_per_item[items[0]] = items[1]
+                mis_decl = re.findall(r"MIS\([\d\D]+\)", pps[0])[0].replace("MIS(", "").replace(")", "")
+                if mis_decl == "rest":
+                    # fill any values in the dictionary that do not have a MIS
+                    for key,value in param_data.mis_per_item.items():
+                        if param_data.mis_per_item[key] == None:
+                            param_data.mis_per_item[key] = float(pps[1])
+                else:
+                    param_data.mis_per_item[mis_decl] = float(pps[1])
             elif "SDC" in pps[0]:
-                data.support_difference_constraint = float(pps[1])
+                param_data.support_difference_constraint = float(pps[1])
             elif "minconf" in pps[0]:
-                data.min_confidence = int(pps[1][:-1]) / 100 # min confidence is given as a percentage
+                param_data.min_confidence = int(pps[1][:-1]) / 100 # min confidence is given as a percentage
 
-    return data
+    # sort the MIS based on value (total order)
+    return param_data
 
 
-def parse_transactions_file(path: str):
-    ...
+def parse_transactions_file(path: str, transaction_db: list, param_data: ParameterData):
+    with open(path) as transactions_file:
+        for line in transactions_file:
+            transaction = "".join(line.split())
+            transaction_db.append(set(transaction.split(",")))
+            for item in transaction.split(","): # insert any items into the param db
+                if item not in param_data.mis_per_item:
+                    param_data.mis_per_item[item] = None
+
+
+def compare_with_key(result: str, check: str):
+    return False
 
 
 def generate_rules():
@@ -60,9 +79,11 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     transaction_db = []
+    params = ParameterData()
     
     # initialize data for apriori
-    # with open(args.transactions) as transactions_file:
-        # transaction = "".join(transactions_file.readline().split()).split(",")
+    parse_transactions_file(args.transactions, transaction_db, params)
+    params_db = parse_params_cfg(args.params, params)
 
-    params_db = parse_params_cfg(args.params)
+    print(transaction_db)
+    print(params)
