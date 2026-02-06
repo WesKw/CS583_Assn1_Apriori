e3349e601e3a7360ee85938c1c08acebb87bc053
WesKw <kwiecinskiwesley@gmail.com>
2026-02-06 14:48:39 -0600

    level 2 candidate generation function

--- msapriori.py
+++ msapriori.py
@@ -7,8 +7,7 @@ from dataclasses import dataclass
 
 @dataclass
 class ItemData:
-    support_count: int = 0
-    tail_count: int = 0
+    count: int = 0
 
 
 @dataclass
@@ -72,11 +71,25 @@ def generate_rules() -> set:
     ...
 
 
-def level2_candidate_generation():
-    ...
+def level2_candidate_generation(level_1_candidates_dict: dict, transaction_db: list, param_db: ParameterData) -> od:
+    candidate_set = od()
+    n_trans = len(transaction_db)
+    candidates = level_1_candidates_dict["seeds"]
+    supports = level_1_candidates_dict["supports"]
+    # candidates = L.items()
+    for idx,candidate in enumerate(candidates):
+        # check MIS count
+        candidate_support = supports[candidate].count / n_trans
+        if candidate_support >= param_db.mis_per_item[candidate[0]]: # check if the candidate meets the minimum support
+            for item in candidates[idx+1:]: # for each item in the candidate list after the current candidate
+                item_support = supports[item].count / n_trans
+                if item_support and (abs(item_support - candidate_support) <= param_db.support_difference_constraint): # if the items meet the SDC then combine and add
+                    candidate_set[(candidate[0], item[0])] = None
+
+    return candidate_set
 
 
-def ms_candidate_generation():
+def ms_candidate_generation(level_k_candidates, param_db: ParameterData) -> set:
     ...
 
 
@@ -92,23 +105,24 @@ def initial_pass(transactions: list, params: ParameterData) -> dict:
         for item in transaction:
             if ((item,)) not in candidate_db:
                 candidate_db[(item,)] = ItemData()
-            candidate_db[(item,)].support_count += 1
+            candidate_db[(item,)].count += 1
 
     # debug print support count
     print("Here are the initial supports for each item...")
     for item in candidate_db:
-        print(f"{item} support = {candidate_db[item].support_count} / {len(transactions)}")
+        print(f"{item} support = {candidate_db[item].count} / {len(transactions)}")
     print("")
 
     print("Getting candidate items through minimum supports...")
     k1_frequent_itemsets = []
     initial_item = None
+    # use tuples to make itemsets hashable
     for item,mis in params.mis_per_item.items():
         # find the first item in the sorted order that meets the minimum support threshold
-        if initial_item == None and candidate_db[(item,)].support_count / len(transactions) >= mis: # what is the minimum support fraction for a given item in the sorted MIS dict
+        if initial_item == None and candidate_db[(item,)].count / len(transactions) >= mis: # what is the minimum support fraction for a given item in the sorted MIS dict
             k1_frequent_itemsets.append((item,))
             initial_item = item
-        elif initial_item != None and candidate_db[(item,)].support_count / len(transactions) >= params.mis_per_item[initial_item]:
+        elif initial_item != None and candidate_db[(item,)].count / len(transactions) >= params.mis_per_item[initial_item]:
             k1_frequent_itemsets.append((item,))
 
     print("Seeds obtained from minimum supports...")
@@ -118,14 +132,14 @@ def initial_pass(transactions: list, params: ParameterData) -> dict:
     return {"seeds": k1_frequent_itemsets, "supports": candidate_db}
 
 
-def msapriori(transaction_db: list, param_db: ParameterData, rho):
+def msapriori(transaction_db: list, param_db: ParameterData):
     param_db.sort_mis_dict_by_value() # first, we sort the mis dict by the values of the minimum supports to create a total order.
     candidates_dict = initial_pass(transaction_db, param_db) # get 1-frequent candidates
     # develop the first set of 1-frquent itemsets
-    frequent_items = {1: set()}
+    frequent_items = od({1: od()})
     for item in candidates_dict["seeds"]:
-        if candidates_dict["supports"][item].support_count / len(transaction_db) >= param_db.mis_per_item[item[0]]:
-            frequent_items[1].add(item)
+        if candidates_dict["supports"][item].count / len(transaction_db) >= param_db.mis_per_item[item[0]]:
+            frequent_items[1][item] = None
 
     # debug
     print("1-frequent itemsets: ")
@@ -135,7 +149,29 @@ def msapriori(transaction_db: list, param_db: ParameterData, rho):
     k_frequency = 2
     last_itemset = frequent_items[k_frequency-1]
     while(len(last_itemset) != 0):
-        ...
+        frequent_items[k_frequency] = od()
+        level_k_candidates = set()
+        # if k is 2 we use a special candidate generation function
+        if k_frequency == 2:
+            level_k_candidates = level2_candidate_generation(candidates_dict, transaction_db, param_db)
+            print("Level 2 candidates:")
+            for candidate in level_k_candidates:
+                print(candidate)
+        else:
+            level_k_candidates = ms_candidate_generation(last_itemset, param_db)
+
+        for transaction in transaction_db:
+            for candidate in level_k_candidates:
+                if candidate in transaction: # if the candidate is in the transaction
+                    ...
+
+                if candidate[1:] in transaction: # if the tail is in the transaction
+                    ...
+
+        # update the frequent candidates list 
+
+        # move to next frequency
+        k_frequency += 1
 
 if __name__ == "__main__":
     parser = ap()
@@ -152,7 +188,7 @@ if __name__ == "__main__":
     params = parse_params_cfg(args.params, params)
     params.sort_mis_dict_by_value()
 
-    msapriori(transaction_db=transaction_db, param_db=params, rho="")
+    msapriori(transaction_db=transaction_db, param_db=params)
 
     print("Ran MS-Apriori with...")
     print("Transactions:")
